0533846708caaba2d61265a49c2cfff3ad14425c
George Glessner <glessneg@mail.gvsu.edu>
2017-02-11 15:58:27 -0500

    Up and Running

--- src/imdb/main.java
+++ src/imdb/main.java
@@ -4,14 +4,19 @@ import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.MovieDb;
 
+
+
 public class main {
+	
 	
 	public static void main(String[] args){
 
-		TmdbApi movies = new TmdbApi("dbae952f0b2a4b3711bf5808e97c4769");
-		//.getMovies();
-		//static MovieDb movie = movies.getMovie(5353, "en");
-		//log("" + movies.getPopularMovies("en", 0).getResults().size());
+		//sTmdbApi movies = new TmdbApi("dbae952f0b2a4b3711bf5808e97c4769");
+		TmdbMovies movies = new TmdbApi("dbae952f0b2a4b3711bf5808e97c4769").getMovies();
+		MovieDb movie = movies.getMovie(5353, "en");
+
+//		static MovieDb movie = movies.getMovie(5353, "en");
+		log("" + movies.getPopularMovies("en", 0).getResults().size());
 	}
 
 	public static void log(String s) {
